6ae2092e6a43b6e22c9fa64577b29f17515c8428
Jay <joaomarcelo1015ja@gmail.com>
2024-07-21 20:32:29 -0300

    adicionando modelos e teste

--- django_mysite/tests/models/test_post.py
+++ django_mysite/tests/models/test_post.py
@@ -1,11 +1,10 @@
 import pytest
-from blog import PostFactory
+from blog.factories import PostFactory
 
 
 @pytest.fixture
 def post_published():
-    return\
-        PostFactory(title='pytest with factory')
+    return PostFactory(title='pytest with factory')
 
 
 @pytest.mark.django_db
